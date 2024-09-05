678ff18fa0381f28190804745f4e1e39d41799b9
menoo-o <menosuper6@gmail.com>
2024-09-05 16:27:51 +0500

    recipe blogs

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -5,34 +5,64 @@ import Grid from '../Components/Grid'
 const blogInfo = [
   {
   id: 1,
-  title: 'How to make healthy oatmeal?',
-  imageUrl: '/blogs/01.webp', // Replace with actual image paths
+  title: 'Chickpea Lentils and Cabbage Falafels',
+  imageUrl: '/recipeblogs/chickpeafalafel.webp', // Replace with actual image paths
   },
   {
   id: 2,
-  title: 'How to make Fruit Salad?',
-  imageUrl: '/blogs/02.jpg', // Replace with actual image paths
+  title: 'Kung Pao with Brown Rice',
+  imageUrl: '/recipeblogs/kungpao.webp', // Replace with actual image paths
   },
   {
   id: 3,
-  title: "Nuts Creamery",
-  imageUrl: 'blogs/03.webp', // Replace with actual image paths
+  title: "Masoor Lentils Special Kebabs",
+  imageUrl: '/recipeblogs/kebab.webp', // Replace with actual image paths
   },
   {
   id: 4,
-  title: 'How to make makhni dal',
-  imageUrl: 'blogs/04.webp', // Replace with actual image paths
+  title: 'Chickpeas With a Guajillo Chili Sauce',
+  imageUrl: '/recipeblogs/chickpeasauce.webp', // Replace with actual image paths
   },
   {
   id: 5,
-  title: 'What to cook for breakfast?',
-  imageUrl: '/blogs/01.webp', // Replace with actual image paths
+  title: 'One Pot Lentil Soup',
+  imageUrl: '/recipeblogs/haleem.webp', // Replace with actual image paths
   },
   {
   id: 6,
-  title: 'Recipes to maintain Nutrition',
-  imageUrl: '/blogs/02.jpg', // Replace with actual image paths
+  title: 'Vanilla Cake',
+  imageUrl: '/recipeblogs/vanillacake.webp', // Replace with actual image paths
   },
+  
+  {
+    id: 7,
+    title: 'Hot Spicy Kidney Beans',
+    imageUrl: '/recipeblogs/spicykidney.webp', // Replace with actual image paths
+    },
+    
+  {
+    id: 8,
+    title: 'Kheer',
+    imageUrl: '/recipeblogs/kheer.webp', // Replace with actual image paths
+    },
+    
+  {
+    id: 9,
+    title: 'Chicken Biryani',
+    imageUrl: '/recipeblogs/biryani.webp', // Replace with actual image paths
+    },
+    
+  {
+    id: 10,
+    title: 'Red Lentil Hummus',
+    imageUrl: '/recipeblogs/hummuslentil.webp', // Replace with actual image paths
+    },
+    
+  {
+    id: 11,
+    title: 'Rice Balls',
+    imageUrl: '/recipeblogs/riceballs.webp', // Replace with actual image paths
+    },
   // Add more blog objects here as needed
   ];
 
